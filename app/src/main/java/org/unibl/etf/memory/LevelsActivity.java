9f14a907569c615d0b88730956f9d51372747663
jelena-pesevski <jpesevski99@gmail.com>
2021-10-12 13:02:55 +0200

    games activities

--- app/src/main/java/org/unibl/etf/memory/LevelsActivity.java
+++ app/src/main/java/org/unibl/etf/memory/LevelsActivity.java
@@ -1,6 +1,8 @@
 package org.unibl.etf.memory;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,20 @@ public class LevelsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levels);
+    }
+
+    public void startFirstLevel(View view){
+        Intent intent=new Intent(LevelsActivity.this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void startScndLevel(View view){
+        Intent intent=new Intent(LevelsActivity.this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void startThirdLevel(View view){
+        Intent intent=new Intent(LevelsActivity.this, GameActivity.class);
+        startActivity(intent);
     }
 }
