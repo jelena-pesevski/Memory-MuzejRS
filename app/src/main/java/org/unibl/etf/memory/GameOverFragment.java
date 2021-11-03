e9fd3226a8c7e14bbcdc0cc8b2fcf9a165ae228a
Milica <milakovicmilica@yahoo.com>
2021-11-03 13:45:34 +0100

    Added confetti

--- app/src/main/java/org/unibl/etf/memory/GameOverFragment.java
+++ app/src/main/java/org/unibl/etf/memory/GameOverFragment.java
@@ -1,12 +1,19 @@
 package org.unibl.etf.memory;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import nl.dionsegijn.konfetti.KonfettiView;
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
 
 public class GameOverFragment extends Fragment {
 
@@ -14,6 +21,26 @@ public class GameOverFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_game_over, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        final KonfettiView konfettiView =getView().findViewById(R.id.viewKonfetti);
+
+        konfettiView.build()
+                .addColors(Color.rgb(236,172,56), Color.rgb(159,57,23),
+                            Color.rgb(214,237,243), Color.rgb(168,198,99))
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.Square.INSTANCE, Shape.Circle.INSTANCE)
+                .addSizes(new Size(12, 5f))
+                .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
+                .streamFor(200, 3000L);
+
     }
 }
